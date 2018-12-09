502148ca7c894a931fa31057b9c4a8d8fa116b52
EvgenyFomin <sevenup96@mail.ru>
2018-12-09 15:48:45 +0300

    addTreePage

---
+++ src/main/java/com/example/treepage/controller/MainController.java
@@ -0,0 +1,10 @@
+package com.example.treepage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+    public String main() {
+        return "index";
+    }
+}
